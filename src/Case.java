dbb73c6253ff0fb3a458b642f31b55e9efff64e6
mertpinarbasi <mpinarbasi35@gmail.com>
2021-06-01 20:54:46 +0300

    Processor and Case
    Processor and case classes are created.In addition to that , .txt files are added.

---
+++ src/Case.java
@@ -0,0 +1,48 @@
+
+public class Case {
+	private String brand;
+	private String model;
+	private int psu;
+	private int price;
+
+	public Case(String brand, String model, int psu, int price) {
+		super();
+		this.brand = brand;
+		this.model = model;
+		this.psu = psu;
+		this.price = price;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getPsu() {
+		return psu;
+	}
+
+	public void setPsu(int psu) {
+		this.psu = psu;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
---
+++ src/Processor.java
@@ -0,0 +1,99 @@
+
+public class Processor {
+	private String brand;
+	private String model;
+	private int core;
+	private int thread;
+	private String socket;
+	private double clockSpeed;
+	private int tdp;
+	private boolean isUnlocked;
+	private int price;
+
+	public Processor(String brand, String model, int core, int thread, String socket, double clockSpeed, int tdp,
+			boolean isUnlocked, int price) {
+		super();
+		this.brand = brand;
+		this.model = model;
+		this.core = core;
+		this.thread = thread;
+		this.socket = socket;
+		this.clockSpeed = clockSpeed;
+		this.tdp = tdp;
+		this.isUnlocked = isUnlocked;
+		this.price = price;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getCore() {
+		return core;
+	}
+
+	public void setCore(int core) {
+		this.core = core;
+	}
+
+	public int getThread() {
+		return thread;
+	}
+
+	public void setThread(int thread) {
+		this.thread = thread;
+	}
+
+	public String getSocket() {
+		return socket;
+	}
+
+	public void setSocket(String socket) {
+		this.socket = socket;
+	}
+
+	public double getClockSpeed() {
+		return clockSpeed;
+	}
+
+	public void setClockSpeed(double clockSpeed) {
+		this.clockSpeed = clockSpeed;
+	}
+
+	public int getTdp() {
+		return tdp;
+	}
+
+	public void setTdp(int tdp) {
+		this.tdp = tdp;
+	}
+
+	public boolean isUnlocked() {
+		return isUnlocked;
+	}
+
+	public void setUnlocked(boolean isUnlocked) {
+		this.isUnlocked = isUnlocked;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
